964e2b1dd3b8d633e28372d1c13a163c59ee375b
Jay Limbasia <jaylimbasia25@gmail.com>
2023-12-25 11:48:09 -0500

    added file for global vars

---
+++ django_ai_codebot/constants.py
@@ -0,0 +1,6 @@
+programming_languages = [
+    'c', 'clike', 'cpp', 'csharp', 'css', 'dart', 'django', 'erlang', 'git', 'go', 'java',
+    'javascript', 'jsx', 'kotlin', 'markup', 'markup-templating', 'objectivec', 'parser',
+    'perl', 'php', 'powershell', 'python', 'r', 'regex', 'ruby', 'sql', 'swift', 'tsx',
+    'typescript', 'yaml',
+]
